11d1cf6b0597cd96a0bd99e4799f0d2a82d241f6
Kamil Marcinow <kamil.marcinow@gmail.com>
2019-01-26 11:57:11 +0100

    first commit

---
+++ UserService.java
@@ -0,0 +1,21 @@
+package SDA;
+
+public class UserService {
+
+    public static void main(String[] args){
+
+        User user = new User();
+        user.setName("jan");
+        user.setLastMame("nowak");
+        user.setLogin("jnowak");
+        user.getPassword("admin");
+
+
+
+
+    }
+
+
+
+
+}
